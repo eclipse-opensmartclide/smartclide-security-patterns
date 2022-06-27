665da927d6f4258c5bf42879b1fe39438dcacaf3
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 09:41:24 +0200

    Added missing copyrights
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
+#*******************************************************************************
+# Copyright (C) 2021-2022 CERTH
+# 
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+# 
+# SPDX-License-Identifier: EPL-2.0
+#*******************************************************************************
+
 import pymongo
 from bson.objectid import ObjectId
 f=open('password.cfg','r')
